70c6bc96a73fc9fbcac2af615c0a2d66cac7154f
riyazriya010 <riyur017@gmail.com.com>
2025-03-29 16:23:01 +0530

    problem TWo pointer 1

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -1,1 +1,21 @@
-console.logO('Hello Vahitha')
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                return [i, j]
+            }
+        }
+    }
+};
+
+const nums = [2,7,11,15], target = 9
+console.log(twoSum(nums, target))
+
+// nums = [3,2,4], target = 6
+// nums = [3,3], target = 6
+
---
+++ 7. Reverse Integer.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let reversedInt = 0
+    while(x > 0){
+        
+    }
+};
+const x = 123
+console.log(reverse(x))
