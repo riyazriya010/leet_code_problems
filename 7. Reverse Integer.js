835f338b6c4240e96126b69d69dd683024b8adae
riyazriya010 <riyur017@gmail.com.com>
2025-03-30 18:10:21 +0530

    7. Reverse integer solved

--- 7. Reverse Integer.js
+++ 7. Reverse Integer.js
@@ -3,10 +3,23 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let reversedInt = 0
+    let minInt = -(2 ** 31)
+    let maxInt = 2 ** 31 -1
+    let reversedInt = ''
+    let isNegative = (x < 0) ? -1 : 1
+    x = Math.abs(x)
     while(x > 0){
-        
+        reversedInt += x % 10
+        x = Math.floor(x / 10)
+        //check  weather the result within the 32 bit integer
+        if(reversedInt < minInt || reversedInt > maxInt) return 0
     }
+    return Number(reversedInt) * isNegative
 };
 const x = 123
 console.log(reverse(x))
+
+
+
+
+
