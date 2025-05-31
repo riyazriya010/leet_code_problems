0f165695746d6ca801be7a81cb7338fccacdecfd
riyazriya010 <riyur017@gmail.com.com>
2025-05-31 18:22:37 +0530

    solved Easy No.9

---
+++ 9. Palindrome Number.js
@@ -0,0 +1,14 @@
+var isPalindrome = function (x) {
+    if(x < 0) return false
+    const original = x
+    let reversed = 0
+    while(x !== 0){
+        let lastDigit = x % 10
+        reversed = (reversed * 10) + lastDigit
+        x = Math.floor(x / 10)
+    }
+    return original === reversed
+}
+
+const num = 121
+console.log(isPalindrome(num))
